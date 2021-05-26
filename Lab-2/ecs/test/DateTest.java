c478b72d7e37f37241d3b354ae932c1eb3c781c2
Patrick <patrick.loranger.24@gmail.com>
2021-05-25 22:03:42 -0400

    DateTest.java Test

--- Lab-2/ecs/test/DateTest.java
+++ Lab-2/ecs/test/DateTest.java
@@ -10,4 +10,119 @@ class DateTest {
     assertEquals(new Date(2020,5,4), d.nextDate());
   }
 
+  @Test
+  void nextDate_tc1_test() {
+    Date d = new Date(1700,6,20);
+    assertEquals(new Date(1700,6,21), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc2_test() {
+    Date d = new Date(2005,4,15);
+    assertEquals(new Date(2005,4,16), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc3_test() {
+    Date d = new Date(1901,7,20);
+    assertEquals(new Date(1901,7,21), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc4_test() {
+    Date d = new Date(3456,3,27);
+    assertEquals(new Date(3456,3,28), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc5_test() {
+    Date d = new Date(1500,2,17);
+    assertEquals(new Date(1500,2,18), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc6_test() {
+    Date d = new Date(1700,6,29);
+    assertEquals(new Date(1700,6,30), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc7_test() {
+    Date d = new Date(1800,11,29);
+    assertEquals(new Date(1800,11,30), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc8_test() {
+    Date d = new Date(3453,1,29);
+    assertEquals(new Date(3453,1,30), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc9_test() {
+    Date d = new Date(444,2,29);
+    assertEquals(new Date(444,3,1), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc10_test() {
+    Date d = new Date(2005,4,30);
+    assertEquals(new Date(2005,5,1), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc11_test() {
+    Date d = new Date(3453,1,30);
+    assertEquals(new Date(3453,1,31), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc12_test() {
+    Date d = new Date(3456,3,30);
+    assertEquals(new Date(3456,3,31), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc13_test() {
+    Date d = new Date(1901,7,31);
+    assertEquals(new Date(1901,8,1), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc14_test() {
+    Date d = new Date(3453,1,31);
+    assertEquals(new Date(3453,2,1), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc15_test() {
+    Date d = new Date(3456,12,31);
+    assertEquals(new Date(3457,1,1), d.nextDate());
+  }
+
+  @Test
+  void nextDate_tc16_test() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1500,2,31));
+  }
+
+  @Test
+  void nextDate_tc17_test() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1500,2,29));
+  }
+
+  @Test
+  void nextDate_tc18_test() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(-1,10,20));
+  }
+
+  @Test
+  void nextDate_tc19_test() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1458,15,12));
+  }
+
+  @Test
+  void nextDate_tc20_test() {
+    assertThrows(IllegalArgumentException.class, () -> new Date(1975,6,-50));
+  }
+
 }
