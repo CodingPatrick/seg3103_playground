5871a39a69a44354a777ce709084afa7574a9877
Akram El-Gaouny <aelga098@uottawa.ca>
2021-06-06 14:46:18 -0400

    Akram El-Gaouny - Fifth commit (passing test). In this commit, I fixed the previous test such that it can pass. The value after reset of any space should be null which is what this case states and tests for

--- Lab-4/tic_java/test/TicTacToeTest.java
+++ Lab-4/tic_java/test/TicTacToeTest.java
@@ -41,6 +41,6 @@ public class TicTacToeTest {
         TicTacToe board = new TicTacToe(3,3);
         board.playNext(1,1);
         board.reset();
-        assertEquals(board.getValue(1,1), "X");
+        assertEquals(board.getValue(1,1), null);
     }
 }
