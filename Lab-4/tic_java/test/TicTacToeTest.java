7a71fd0581d0218bb66705bc0746880596b99f40
Akram El-Gaouny <aelga098@uottawa.ca>
2021-06-06 13:30:19 -0400

    Akram El-Gaouny (Commit -1 -  Passing Test) This commit is for the passing test for TicTacToe_tc1, it expects an exception and that's what it got

--- Lab-4/tic_java/test/TicTacToeTest.java
+++ Lab-4/tic_java/test/TicTacToeTest.java
@@ -1,14 +1,14 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
 public class TicTacToeTest {
 
     @Test
-    void TicTacToe_tc1(){
-        // testing the default
-        TicTacToe board = new TicTacToe(0,0);
-        assertEquals(board.toString(), "No Board exists");
+    public void TicTacToe_tc1(){
+        // testing the default 
+        assertThrows(IllegalArgumentException.class, () -> new TicTacToe(0,0) );
     }
     
 }
